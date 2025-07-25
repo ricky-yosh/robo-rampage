7d2c347c9d26a4974ef7a02c969f4d2ff08df835
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-07-24 23:00:11 -0700

    Add starting code for navigation system

--- robo-rampage/Player/player.gd
+++ robo-rampage/Player/player.gd
@@ -2,10 +2,13 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+
+@export var jump_height: float = 1.0
+@export var fall_multiplier: float = 2.0
 
 var mouse_motion := Vector2.ZERO
 @onready var camera_pivot: Node3D = $CameraPivot
+var gravity3d = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED # When player loads in, set input mode for the mouse. Mouse is invisible and any code that listens to mouse will always listen to it
@@ -14,11 +17,14 @@ func _physics_process(delta: float) -> void:
 	handle_camera_rotation()
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		if velocity.y >= 0:
+			velocity += get_gravity() * delta
+		else:
+			velocity.y -= gravity3d * delta * fall_multiplier
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = sqrt(jump_height * 2.0 * gravity3d)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
