6767541e91f3fdb312df82ae06a176884dbed7d1
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-07-26 17:23:28 -0700

    Create particles where raycast collides with objects

--- robo-rampage/Weapons/hitscan_weapon.gd
+++ robo-rampage/Weapons/hitscan_weapon.gd
@@ -5,6 +5,7 @@ extends Node3D
 @export var weapon_mesh: Node3D
 @export var weapon_damage := 15
 @export var muzzle_flash: GPUParticles3D
+@export var sparks: PackedScene
 
 @onready var cooldown_timer: Timer = $CooldownTimer
 @onready var weapon_position: Vector3 = weapon_mesh.position
@@ -27,3 +28,6 @@ func shoot() -> void:
 	weapon_mesh.position.z += recoil
 	if collider is Enemy:
 		collider.hitpoints -= weapon_damage
+	var spark = sparks.instantiate()
+	add_child(spark)
+	spark.global_position = ray_cast_3d.get_collision_point()
