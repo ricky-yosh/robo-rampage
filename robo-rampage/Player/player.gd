260c661f1ba0e59f1610fc5dab18b370f0cc4e2f
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-07-29 21:51:59 -0700

    Add new weapon swapping and ammo count

--- robo-rampage/Player/player.gd
+++ robo-rampage/Player/player.gd
@@ -40,8 +40,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = sqrt(jump_height * 2.0 * gravity3d)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get the input direction and handle the mov ice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
