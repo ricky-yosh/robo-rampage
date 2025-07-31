e722c5568ab7525b026067fad18a79890b783b40
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-07-30 22:03:28 -0700

    Create ammo handler system

--- robo-rampage/Player/player.gd
+++ robo-rampage/Player/player.gd
@@ -10,6 +10,7 @@ const SPEED = 5.0
 @onready var camera_pivot: Node3D = $CameraPivot
 @onready var damage_animation_player: AnimationPlayer = $DamageTexture/DamageAnimationPlayer
 @onready var game_over_menu: Control = $GameOverMenu
+@onready var ammo_handler: AmmoHandler = $SubViewportContainer/SubViewport/WeaponCamera/AmmoHandler
 
 var mouse_motion := Vector2.ZERO
 var gravity3d = ProjectSettings.get_setting("physics/3d/default_gravity")
