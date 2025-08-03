10f247341d0103e2f74b9966076d080efb572cd8
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-08-02 19:51:39 -0700

    Complete robo-rampage

--- robo-rampage/Weapons/hitscan_weapon.gd
+++ robo-rampage/Weapons/hitscan_weapon.gd
@@ -34,10 +34,10 @@ func shoot() -> void:
 		muzzle_flash.restart()
 		cooldown_timer.start(1.0 / fire_rate)
 		var collider = ray_cast_3d.get_collider()
-		printt("weapon fired! ", collider)
 		weapon_mesh.position.z += recoil
-		if collider is Enemy:
-			collider.hitpoints -= weapon_damage
-		var spark = sparks.instantiate()
-		add_child(spark)
-		spark.global_position = ray_cast_3d.get_collision_point()
+		if ray_cast_3d.is_colliding():
+			if collider is Enemy:
+				collider.hitpoints -= weapon_damage
+			var spark = sparks.instantiate()
+			add_child(spark)
+			spark.global_position = ray_cast_3d.get_collision_point()
